57ac44974c7208d08e4e646882d804f929118d0b
aba23913 <adriaan.bakker@ordina.nl>
2020-08-10 13:55:19 +0200

    added guard on routing condition

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from '../product';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +10,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail...';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
+    console.log(this.route.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      description: 'Leaf rake with 48-inch wooden handle',
+      imageUrl: 'assets/images/leaf_rake.png',
+      price: 19.95,
+      productCode: 'GDN-0011',
+      productId: id,
+      productName: 'Leaf Rake',
+      releaseDate: 'March 19, 2019',
+      starRating: 3.2
+    };
+  }
+
+  onBack(): void {
+      this.router.navigate (['/products']);
   }
 
 }
